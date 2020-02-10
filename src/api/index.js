eff6dd1939635846d3551c8dd1fad4fb9f7cff1a
junjiezeng <zengjunj21@sina.com>
2020-02-10 11:42:38 +0800

    跨域配置

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,13 @@
 
 */
 import ajax from './ajax.js'
+const BASE_URL = '/api'
 // 根据经纬度获取位置详情
-export const reqAddress = (geohash) => ajax(`/position/${geohash}`)
+export const reqAddress = (geohash) => ajax(BASE_URL + `/position/${geohash}`)
 
 // 获取食品分类列表
-export const reqFoodTypes = () => ajax('/index_category');
+export const reqFoodTypes = () => ajax(BASE_URL + '/index_category');
 
 // 根据经纬度获取商铺列表
-export const reqShopList = (longitude,latitude) => ajax('/shops',{longitude,latitude})
+export const reqShopList = (longitude,latitude) => ajax(BASE_URL + '/shops',{longitude,latitude})
 
